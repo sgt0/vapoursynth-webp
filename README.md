ef958fd5966c9e8bac7a02d47fe34d317c81c52a
sgt0 <140186177+sgt0@users.noreply.github.com>
2024-11-23 19:50:26 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,108 @@
+# vapoursynth-webp
+
+WebP encoding as a [VapourSynth][] plugin. Currently does VP8L lossless encoding.
+
+## Install
+
+Manually: download a release from the [Releases][] page and unzip
+`webp.dll` (Windows), `libwebp.so` (Linux), or `libwebp.dylib` (macOS) into a
+[plugins directory][plugin-autoloading]. There are separate artifacts for Raptor
+Lake (`*-raptorlake.zip`) and AMD Zen 4 (`*-znver4.zip`) CPUs which may or may
+not have better performance than the plain x86_64 build.
+
+## API
+
+```python
+webp.WebP(
+    clip: vs.VideoNode,
+    path: str,
+    parents: bool = False,
+)
+```
+
+- `clip` — Input video node. Must have RGB24 format.
+- `path` — Output file path. Supports [Rust-like formatting][]. The frame number
+  `n` is the only available parameter.
+- `parents` - If `True`, any missing parents of `path` are created as needed.
+  Otherwise, a missing parent will throw an error.
+
+Writes each requested frame to `path`.
+
+## Examples
+
+Write frame 5000 to `output.webp`:
+
+```python
+from vskernels import Bilinear
+from vssource import BestSource
+from vstools import core, vs
+
+clip = BestSource.source(r"/path/to/video.mkv")
+clip = Bilinear.resample(clip, format=vs.RGB24)
+
+core.webp.WebP(clip, path="output.webp").get_frame(5000)
+```
+
+Write 10 frames to `output_N.webp`, where `N` is the frame number
+(`output_5000.webp`, `output_5001.webp`, etc.):
+
+```python
+from vskernels import Bilinear
+from vssource import BestSource
+from vstools import clip_async_render, core, vs
+
+clip = BestSource.source(r"/path/to/video.mkv")
+clip = Bilinear.resample(clip, format=vs.RGB24)
+
+webp = core.webp.WebP(clip, path=r"output_{n}.webp")
+clip_async_render(webp[5000:5010])
+```
+
+Because Rust-like formatting is supported, things like padding work as well:
+
+```python
+# Pad to 5 digits with 0s (`output_05000.webp`, `output_05001.webp`, etc.)
+core.webp.WebP(clip, path=r"output_{n:05}.webp")
+
+# Include the frame number twice (`output_5000_5000.webp`, `output_5001_5001.webp`, etc.)
+core.webp.WebP(clip, path=r"output_{n}_{n}.webp")
+```
+
+Like Python's pathlib, by default an error will be thrown if any of `path`'s
+parents do not exist. This filter can optionally create any missing parents with
+the `parents` parameter:
+
+```python
+# This will throw an error.
+core.webp.WebP(clip, path=r"/nonexistent/path/output.webp")
+
+# But this will create the missing parents.
+core.webp.WebP(clip, path=r"/nonexistent/path/output.webp", parents=True)
+
+# This also enables patterns like creating a folder per frame.
+core.webp.WebP(clip, path=r"screenshots/frame_{n}/output.webp", parents=True)
+```
+
+## Build
+
+Rust v1.83.0-nightly and cargo may be used to build the project. Older versions
+will likely work fine but they aren't explicitly supported.
+
+```bash
+$ git clone https://github.com/sgt0/vapoursynth-webp.git
+$ cd vapoursynth-webp
+
+# Debug build.
+$ cargo build
+
+# Release (optimized) build.
+$ cargo build --release
+
+# Release build optimized for the host CPU.
+$ RUSTFLAGS="-C target-cpu=native" cargo build --release
+```
+
+[VapourSynth]: https://www.vapoursynth.com
+[Releases]: https://github.com/sgt0/vapoursynth-webp/releases
+[plugin-autoloading]: https://www.vapoursynth.com/doc/installation.html#plugin-autoloading
+[Rust-like formatting]: https://doc.rust-lang.org/std/fmt/index.html#formatting-parameters
---
+++ src/lib.rs
@@ -0,0 +1,159 @@
+use std::{
+  ffi::{c_void, CStr},
+  fs::{create_dir_all, File},
+  path::Path,
+};
+
+use const_str::cstr;
+use formatx::formatx;
+use image_webp::{ColorType, WebPEncoder};
+use vapours::frame::VapoursVideoFrame;
+use vapoursynth4_rs::{
+  core::CoreRef,
+  declare_plugin,
+  frame::{FrameContext, VideoFrame},
+  key,
+  map::MapRef,
+  node::{
+    ActivationReason, Dependencies, Filter, FilterDependency, Node, RequestPattern, VideoNode,
+  },
+  ColorFamily, SampleType,
+};
+
+/// WebP encoding as a VapourSynth plugin. Currently does VP8L lossless
+/// encoding.
+struct WebPFilter {
+  /// Input video node.
+  node: VideoNode,
+
+  /// Output file path. Supports Rust-like formatting.
+  path: String,
+
+  /// If `true`, any missing parents of `path` are created as needed.
+  /// Otherwise, a missing parent will throw an error.
+  parents: bool,
+}
+
+impl Filter for WebPFilter {
+  type Error = &'static CStr;
+  type FrameType = VideoFrame;
+  type FilterData = ();
+
+  fn create(
+    input: &MapRef,
+    output: &mut MapRef,
+    _data: Option<Box<Self::FilterData>>,
+    mut core: CoreRef<'_>,
+  ) -> Result<(), Self::Error> {
+    let Ok(node) = input.get_video_node(key!("clip"), 0) else {
+      return Err(cstr!("Failed to get clip."));
+    };
+
+    let n = node.clone();
+    let vi = n.info();
+
+    if vi.format.bits_per_sample != 8
+      || vi.format.color_family != ColorFamily::RGB
+      || vi.format.sample_type != SampleType::Integer
+    {
+      return Err(cstr!("Only RGB24 input is supported."));
+    }
+
+    let path = input
+      .get_utf8(key!("path"), 0)
+      .expect("Missing required `path` parameter.");
+
+    let parents = input.get_int(key!("parents"), 0).unwrap_or(0) == 1;
+
+    let mut filter = Self {
+      node,
+      path: path.to_owned(),
+      parents,
+    };
+
+    let deps = [FilterDependency {
+      source: filter.node.as_mut_ptr(),
+      request_pattern: RequestPattern::StrictSpatial,
+    }];
+
+    core.create_video_filter(
+      output,
+      cstr!("WebP"),
+      vi,
+      Box::new(filter),
+      Dependencies::new(&deps).unwrap(),
+    );
+
+    Ok(())
+  }
+
+  fn get_frame(
+    &self,
+    n: i32,
+    activation_reason: ActivationReason,
+    _frame_data: *mut *mut c_void,
+    mut ctx: FrameContext,
+    _core: CoreRef<'_>,
+  ) -> Result<Option<VideoFrame>, Self::Error> {
+    match activation_reason {
+      ActivationReason::Initial => {
+        ctx.request_frame_filter(n, &self.node);
+      }
+      ActivationReason::AllFramesReady => {
+        let src = self.node.get_frame_filter(n, &mut ctx);
+        let r_channel = src.as_slice::<u8>(0);
+        let g_channel = src.as_slice::<u8>(1);
+        let b_channel = src.as_slice::<u8>(2);
+        let width = src.frame_width(0) as usize;
+        let height = src.frame_height(0) as usize;
+
+        // Naive RGB packing.
+        let mut packed = vec![0; width * height * 3];
+        for (((src, r), g), b) in packed
+          .chunks_exact_mut(3)
+          .zip(r_channel.iter())
+          .zip(g_channel.iter())
+          .zip(b_channel.iter())
+        {
+          src[0] = *r;
+          src[1] = *g;
+          src[2] = *b;
+        }
+
+        let formatted_path =
+          formatx!(self.path.clone(), n = n).expect("`path` format string was invalid");
+        let output_path = Path::new(formatted_path.as_str());
+
+        if self.parents {
+          if let Some(parent) = output_path.parent() {
+            create_dir_all(parent).expect("Failed to create output directory");
+          }
+        }
+
+        let encoder = WebPEncoder::new(File::create(output_path).unwrap());
+        encoder
+          .encode(&packed, width as u32, height as u32, ColorType::Rgb8)
+          .expect("Failed to encode to WebP");
+
+        return Ok(Some(src));
+      }
+      ActivationReason::Error => {}
+    }
+
+    Ok(None)
+  }
+
+  const NAME: &'static CStr = cstr!("WebP");
+  const ARGS: &'static CStr = cstr!("clip:vnode;path:data;parents:int:opt;");
+  const RETURN_TYPE: &'static CStr = cstr!("clip:vnode;");
+}
+
+declare_plugin!(
+  "sgt.webp",
+  "webp",
+  "WebP encoder.",
+  (1, 0),
+  VAPOURSYNTH_API_VERSION,
+  0,
+  (WebPFilter, None)
+);
